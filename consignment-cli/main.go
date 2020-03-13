3c1ed6923513734df8aa544fe423f770abb2dd4f
Erick Njoroge <erick.njoroge@betika.com>
2020-03-13 21:28:49 +0300

    added consignment cli service to test creating consignment

---
+++ consignment-cli/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/ericnjoroge/shippy-microservices/consignment-service/proto/consignment"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	// Setup connection to the server
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("[Consignment CLI] Unable to connect to the gRPC server: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewShippingServiceClient(conn)
+
+	// Contact the server and print out itd details
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("[Consignment CLI] Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("[Consignment CLI] Could not create consignment: %v", err)
+	}
+	log.Printf("Created consignment: %t", r.Created)
+}
